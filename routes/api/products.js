24928e25ada4752aec6bc5cc5be70aba186d55c1
sidhi-sid <sidhisidh469@gmail.com>
2020-06-09 01:13:15 +0530

    products in api

--- routes/api/products.js
+++ routes/api/products.js
@@ -2,8 +2,23 @@ const route=require('express').Router()
 const Product=require('../../db').product
 route.get('/',(req,res)=>{
     //get all products
+    Product.findAll()
+    .then((products)=>{
+        res.status(200).send(products)
+    })
+    .catch((err)=>{
+        res.status(500).send({
+            error:"Could not retrieve products"
+        })
+    })
 })
 route.post('/',(req,res)=>{
     //add a new product
+    Product.create({
+        name:req.body.name,
+        manufacturer:req.body.manufacturer,
+        price:parseFloat(req.body.price)
+
+    })
 })
 exports=module.exports=route
