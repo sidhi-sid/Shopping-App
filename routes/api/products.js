b5497392a3465381059d485be2e46142e930a103
sidhi-sid <sidhisidh469@gmail.com>
2020-06-09 21:22:30 +0530

    backend done

--- routes/api/products.js
+++ routes/api/products.js
@@ -13,12 +13,24 @@ route.get('/',(req,res)=>{
     })
 })
 route.post('/',(req,res)=>{
+    //validate the product
+    if(isNaN(req.body.price)){  //isNaN is a function to check if no is not a number
+        return res.status(403).send({
+            error:"Price is not valid "
+        })
+    }
     //add a new product
     Product.create({
         name:req.body.name,
         manufacturer:req.body.manufacturer,
         price:parseFloat(req.body.price)
 
+    }).then((product)=>{
+        res.status(201).send(product)
+    }).catch((err)=>{
+        res.status(501).send({
+            error:"Error adding product"
+        })
     })
 })
 exports=module.exports=route
